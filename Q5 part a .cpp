ca6f8f6f53a039405888d94d4ead486bcbc14ee5
Aayushi-Uniyal <auniyal_be24@thapar.edu>
2025-09-17 23:30:18 +0530

    Update and rename Q5.cpp part a to Q5 part a .cpp

--- Q5 part a .cpp
+++ Q5 part a .cpp
@@ -7,13 +7,16 @@ class StackTwoQ {
 
 public:
     void push(int x) {
+        
         q2.push(x);
+
+        
         while (!q1.empty()) {
             q2.push(q1.front());
             q1.pop();
         }
 
-
+       
         swap(q1, q2);
     }
 
